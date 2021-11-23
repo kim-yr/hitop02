405c3c8ce15e32fadcf11cf7f074cce642e96d95
kim-yr <dPfud1138@gmail.com>
2021-11-23 18:11:02 +0900

    21/11/23 gnb 풀다운

--- pc/js/main.js
+++ pc/js/main.js
@@ -9,3 +9,17 @@ const mainVisualSlider = new Swiper("#mainVisual", {
   },
 });
 //f(x,y) = x+y*3; f(3,5) = 18
+
+//document.querySelectoraAll : 배열처럼 생긴 nodelist를 return 함.
+const gnbItem = document.querySelectorAll("#gnb .list > li");
+const header = document.querySelector("#header");
+console.log(gnbItem);
+console.log(gnbItem.length);
+for (let i = 0; i < 5; i++) {
+  gnbItem[i].addEventListener("mouseenter", function () {
+    header.classList.add("open");
+  });
+  gnbItem[i].addEventListener("mouseleave", function () {
+    header.classList.remove("open");
+  });
+}
