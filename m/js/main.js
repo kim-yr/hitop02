54bc5a787081e740c5e07d567eaf85e59ea56369
장성호 <jjang051@hanmail.net>
2021-12-08 12:20:36 +0900

    jquery

--- m/js/main.js
+++ m/js/main.js
@@ -18,27 +18,59 @@ const newsSlider = new Swiper("#news .listBox", {
 
 //f(x,y) = x+y*3; f(3,5) = 18
 
-//변수로 인식을 시키겠다. 정의하는 거
-//const btnAll
-const pi = 3.14;
-let x = 10;
-let myWeight = 80;
-
-myWeight = myWeight + 10;
-x = x + 10;
-x = pi + 10;
-console.log(x);
-
-//1. 찾기...
-//2. 찾은 것에 이벤트 걸기...
-//3. gnb에 on class 달기...
+/*
 const btnAll = document.querySelector(".all");
 const gnb = document.querySelector("#gnb");
-console.log(btnAll);
 btnAll.addEventListener("click", function () {
-  //alert("all 을 눌렀습니다.");
   gnb.classList.toggle("on");
   btnAll.classList.toggle("on");
-  //add / remove
 });
-//alert("나는 바로뜨는 경고창입니다.");
+*/
+// jQuery  ===  $
+// js 표준으로 자리잡기 전에... 웹개발 힘들었음....
+const header = $("#header");
+const btnAll = $(".all");
+const gnb = $("#gnb");
+const depth02 = gnb.find(".list > li > .depth02 ");
+const depth01 = gnb.find(".list > li > a");
+
+//const header01 = document.querySelector("#header");
+
+btnAll.on("click", function () {
+  //jQuery class관련한 4가지 메서드를 제공한다.  addClass, removeClass, hasClass, toggleClass
+  gnb.toggleClass("on");
+  btnAll.toggleClass("on");
+  depth02.stop().delay(500).slideUp();
+});
+//a태그에 이벤트 걸때는 e.preventDefault() 를 쓰던지  아니면 마지막 줄에 return을 쓴다.
+//자바스크립트의 this context환경에 따라 달라진다.
+
+//dom제어 스크립트는 일단 잘찾아야 한다.
+//찾은 다음 조작
+depth01.on("click", function (e) {
+  console.log("aaa");
+  const selectedDepth02 = $(this).siblings(".depth02");
+  const parentDepth02 = $(this).parent().siblings().find(".depth02");
+  parentDepth02.stop().slideUp();
+  selectedDepth02.stop().slideToggle(); //  show() 메서드는 display를 block으로 바꿔 준다.  hide() 는 display를 none으로 바꿔준다. toggle()
+  return false;
+});
+
+//syntax error  문법적 오류
+//not a function  메서드나 내가 만든 함수의 이름이 틀린경우
+//not defined   변수가 정의되어 있지 않거나 변수 scope를 챙겨봐야 한다.
+
+//bom
+$(window).on("scroll", function () {
+  //console.log($(window).scrollTop());
+  const st = $(window).scrollTop();
+  if (st > 0) {
+    if (!header.hasClass("scroll")) {
+      header.addClass("scroll");
+    }
+  } else {
+    if (header.hasClass("scroll")) {
+      header.removeClass("scroll");
+    }
+  }
+});
--- pc/js/jquery_main.js
+++ pc/js/jquery_main.js
@@ -10,8 +10,8 @@ const mainVisualSlider = new Swiper("#mainVisual", {
     nextEl: "#mainVisual .next",
   },
 });
-//f(x,y) = x+y*3; f(3,5) = 18
 
+/*
 const gnbItem = document.querySelectorAll("#gnb .list > li");
 //document.querySelectorAll(찾는거) // 배열처럼 생긴 nodeList를 리턴한다.
 const header = document.querySelector("#header");
@@ -25,28 +25,42 @@ for (let i = 0; i < gnbItem.length; i++) {
     header.classList.remove("open");
   });
 }
+*/
 
 gsap.from("#mainVisual .slogan .main .char", { opacity: 0, x: 150, ease: "power4", duration: 1, stagger: 0.1 });
 gsap.from("#mainVisual .slogan .sub .char", { opacity: 0, x: 150, ease: "power4", duration: 1, delay: 2, stagger: 0.02 });
 
-/**
-const iq = 182;
-let height = 180;
-height = height + 5; // 대입연산자...
-let 나의키 = 180; //특수문자는 두개만 _ , $ 허용한다. 띄어쓰기 안된다. 숫자는 뒤에만 쓸 수 있다. 예약어는 안됨.
-console.log(나의키);
+const gnbList = $("#gnb .list > li");
 
-console.log(10 + 10);
-console.log(10 - 10);
-console.log(10 * 10);
-console.log(10 / 3); // 실수 나누기...(중학생 나누기)
-console.log(10 % 3); // 마너지 구하기...(토등학생 나누기)
+const header = $("#header");
 
-//Number, Stringm, Boolean
-let isOk = false;
-
-//문 statement(조건, 반복)   expression
-if (isOk) {
-  console.log("나는 참이어서 출력됩니다.");
-}
+/*
+// fulldown 메뉴
+gnbList.on("mouseenter", function () {
+  header.addClass("open");
+});
+gnbList.on("mouseleave", function () {
+  header.removeClass("open");
+});
 */
+gnbList.on("mouseenter", function () {
+  const selectedDepth02 = $(this).find(".depth02");
+  selectedDepth02.stop().slideDown(200);
+});
+gnbList.on("mouseleave", function () {
+  const selectedDepth02 = $(this).find(".depth02");
+  selectedDepth02.stop().slideUp(100);
+});
+$(window).on("scroll", function () {
+  //console.log($(window).scrollTop());
+  const st = $(window).scrollTop();
+  if (st > 0) {
+    if (!header.hasClass("scroll")) {
+      header.addClass("scroll");
+    }
+  } else {
+    if (header.hasClass("scroll")) {
+      header.removeClass("scroll");
+    }
+  }
+});
